091311ecdf3ba03dbcd8210dc6698c4f39493341
rsincak <richard.sincak@t-systems.com>
2020-02-16 12:28:31 +0100

    books and users added

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    // ember-cli-build.js
+    sassOptions: {
+    extension: 'scss'
+  }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +23,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  app.import('bower_components/jquery/dist/jquery.min.js');
+  app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
 
   return app.toTree();
 };
